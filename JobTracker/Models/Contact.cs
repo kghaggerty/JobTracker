342aae44fb1711aaab033d3ccc3a575d6d66e968
kevin haggerty <kghaggerty@gmail.com>
2018-03-01 14:57:54 -0600

    Contact model added

--- JobTracker/Models/Contact.cs
+++ JobTracker/Models/Contact.cs
@@ -1,6 +1,17 @@
-﻿namespace JobTracker.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JobTracker.Models
 {
     public class Contact
     {
+        [Key]
+        public int ContactId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string PhoneNumber { get; set; }
+
     }
 }
