fed4969ca95971385044e4cdbe453c8680908b5c
kevin haggerty <kghaggerty@gmail.com>
2018-03-01 14:55:09 -0600

    Company model created

--- JobTracker/Models/Company.cs
+++ JobTracker/Models/Company.cs
@@ -1,6 +1,15 @@
-﻿namespace JobTracker.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JobTracker.Models
 {
     public class Company
     {
+
+        [Key]
+        public int CompanyId { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
     }
 }
